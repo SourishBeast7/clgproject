fe13b6c89afacd030bb6753cfaaa0fb035fe6a3e
SourishBeast7 <aayushpal.singh12@gmail.com>
2024-12-17 15:53:18 +0530

    "Added ditricts and blood types"

--- app/components/MainPage/page.js
+++ app/components/MainPage/page.js
@@ -2,22 +2,31 @@ import React from 'react'
 import Navbar from '../Navbar/page'
 import Footer from '../Footer/page'
 import SearchBox from '../SearchBox/page'
-import {promises as fs} from 'fs'
-
+import { promises as fs } from 'fs'
+import img from "/public/red_blood.jpg"
 
 const MainPage = async () => {
-  let file = await fs.readFile(process.cwd() + '/app/api/data/states/data.json','utf-8')
+  let file = await fs.readFile(process.cwd() + '/app/api/data/states/data.json', 'utf-8')
   let data = JSON.parse(file)
+  const styling = {
+    backgroundImage: `url(${img.src})`,
+    width: "100%",
+    height: "100%",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover"
+  }
   return (
-    <div className='bg-slate-400 min-h-screen'>
-      <div className='flex py-3 px-4 bg-rose-800'>
-        <Navbar />
-      </div>
-      <main className='bg-whi-800 min-h-screen min-w-full'>
-        <div className='input-box border-4 rounded-lg overflow-hidden border-blue-600 m-auto mt-16 w-[50vw] h-[59vh] bg-red-900'>
-          <SearchBox data={data}/>
+    <div className='min-w-full min-h-screen'>
+      <div style={styling}>
+        <div className='flex py-3 px-4 bg-rose-800'>
+          <Navbar />
         </div>
-      </main>
+        <main className='min-h-screen min-w-full'>
+          <div className='input-box border-4 rounded-lg overflow-hidden border-blue-600 m-auto mt-16 w-[50vw] h-[59vh] bg-red-900'>
+            <SearchBox data={data} />
+          </div>
+        </main>
+      </div>
       <Footer />
     </div>
   )
--- app/components/SearchBox/page.js
+++ app/components/SearchBox/page.js
@@ -6,34 +6,52 @@ import { useRef, useState } from 'react'
 export default function Search(data) {
 
   const stateRef = useRef("")
-  const [State, setState] = useState("")
+  const districtRef = useRef("")
+  const [District, setDistrict] = useState([])
   let states = []
+  const handleChange = () => {
+    let s = stateRef.current.value
+    setDistrict(data.data[s])
+  }
+  let bloodTypes = ["A+","A-","B+","B-","AB+","AB-","O+","O-"]
+  
   for (let i in data.data) {
     states.push(i)
   }
-  console.log(states)
+  states.sort()
+  District.sort()
+
+  // for(let i of data.data){
+  //   if(stateRef.current.value===i){
+  //       districts = i;
+  //   }
+  // }
   const searchBloodBank = () => {
-    if (stateRef.current.value === "--Select State or Union Territory--") {
+    if (stateRef.current.value === "--Select State or Union Territory--" || districtRef.current.value === "--Select District--") {
       alert("Enter details correctly")
       return;
     }
-    console.log(stateRef.current.value)
   }
   return (
-    <Form action={searchBloodBank} formMethod='POST' >
-
-      <div className='flex flex-col items-center'>
+    <Form action={searchBloodBank}>
+      <h2 className='pt-[6vh] text-[50px] text-center'>Blood Availability Search</h2>
+      <div className='flex mx-[12vw] mt-[10vh] items-center'>
         {/* <input className='text-black rounded-xl placeholder:text-center placeholder:font-semibold' placeholder='' type='text'/> */}
-        <select ref={stateRef} className='text-black'>
+        <select ref={stateRef} className='text-black font-semibold p-2 rounded-sm'>
           <option>--Select State or Union Territory--</option>
-          {states.map((item) => { return <option value={item}>{item}</option> })}
+          {states.map((item) => { return <option key={item} value={item} >{item}</option> })}
         </select>
-        <select className='text-black'>
-          <option>Select District</option>
-          { }
+        <select onClick={handleChange} ref={districtRef} className='p-2 w-[10vw] font-semibold text-black rounded-sm mx-2'>
+          <option>--Select District--</option>
+          {District.map((item) => { return <option key={item} value={item} >{item}</option> })}
         </select>
-        {/* <input className='text-black rounded-xl placeholder:text-center placeholder:font-semibold' ref={search_query} placeholder='Blood Type' onChange={()=>{setval(search_query.current.value)}} value={val} name="query" /> */}
-        <button className='bg-lime-600 font-bold w-16 rounded-xl mx-auto' type="submit">Submit</button>
+      </div>
+      <div className='flex flex-col'>
+        <select className='text-black mx-auto mt-5 font-semibold p-2 rounded-sm'>
+          <option className='text-center' key={"Null"} value={null}>--Select Blood Type--</option>
+          {bloodTypes.map((e)=>{return <option key={e} value={e}>{e}</option>})}
+        </select>
+        <button className='bg-lime-600 font-bold rounded-md mt-3 p-2 mx-auto' type="submit">Submit</button>
       </div>
     </Form>
   )
