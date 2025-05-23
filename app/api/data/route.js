34e0af5a0d752dc842ef17e4659627ff880c953d
SourishBeast7 <101741593+SourishBeast7@users.noreply.github.com>
2025-05-23 14:42:01 +0530

    ""

--- app/api/data/route.js
+++ app/api/data/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 // import { promises as fs } from 'fs'
-import data from 'data.js'
+import data from './data.js'
 export async function GET(request) {
     try {
         return NextResponse.json(data)
